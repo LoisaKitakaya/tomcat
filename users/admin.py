8137d2f0058764d5e983ab459073ca6863f904ef
Freedom Loisa <kitakayaloisa@gmail.com>
2023-03-26 10:04:49 +0300

    changed user and profile models to implement 2FA

--- users/admin.py
+++ users/admin.py
@@ -4,7 +4,41 @@ from django.contrib.auth.admin import UserAdmin
 
 # Register your models here.
 
-admin.site.register(User, UserAdmin)
+
+class CustomUserAdmin(UserAdmin):
+
+    fieldsets = (
+        (None, {"fields": ("username", "password")}),
+        (
+            "Personal info",
+            {"fields": ("first_name", "last_name", "email", "phone_number")},
+        ),
+        (
+            "Permissions",
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                )
+            },
+        ),
+        ("Important dates", {"fields": ("last_login", "date_joined")}),
+    )
+
+    list_display = (
+        "username",
+        "first_name",
+        "last_name",
+        "email",
+        "phone_number",
+        "is_staff",
+    )
+
+
+admin.site.register(User, CustomUserAdmin)
 
 
 @admin.register(Profile)
--- users/models.py
+++ users/models.py
@@ -1,12 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.core.management.utils import get_random_secret_key
 
 # Create your models here.
 
 
 class User(AbstractUser):
 
-    pass
+    phone_number = models.CharField(max_length=50)
 
 
 class Profile(models.Model):
@@ -20,9 +21,10 @@ class Profile(models.Model):
         (STANDARD, "standard"),
         (PRO, "pro"),
     )
-    
+
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     tier = models.CharField(max_length=20, choices=PROFILE_TIERS, default=FREE)
+    secret_key = models.CharField(max_length=100, default=str(get_random_secret_key()))
     account_limit = models.IntegerField(default=1)
     budget_limit = models.IntegerField(default=4)
     pdf_gen = models.BooleanField(default=False)
