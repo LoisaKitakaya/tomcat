35bd8785e8f4f9c6e39ba11bdeb3756dcdaae5f7
Freedom Loisa <kitakayaloisa@gmail.com>
2023-04-16 17:04:34 +0300

    minor changes

--- billing/views.py
+++ billing/views.py
@@ -26,9 +26,9 @@ def notifications(request):
 
 def pesapal_ipn_callback(request):
     if request.method == "POST":
-        order_tracking_id = request.GET.get("OrderTrackingId")
-        order_merchant_reference = request.GET.get("OrderMerchantReference")
-        order_notification_type = request.GET.get("OrderNotificationType")
+        order_tracking_id = request.POST.get("OrderTrackingId")
+        order_merchant_reference = request.POST.get("OrderMerchantReference")
+        order_notification_type = request.POST.get("OrderNotificationType")
 
         print(f"{order_tracking_id}\n")
         print(f"{order_merchant_reference}\n")
