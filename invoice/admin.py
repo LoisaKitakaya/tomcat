8e5d5385db11b5c9577aaa671f220c0511d09cdd
Freedom Loisa <kitakayaloisa@gmail.com>
2023-06-06 04:49:26 +0300

    new app migrations

--- invoice/admin.py
+++ invoice/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from invoice.models import PaymentAccount, ClientInformation, Invoice
 
-# Register your models here.
+
+@admin.register(PaymentAccount)
+class PaymentAccountAdminView(admin.ModelAdmin):
+    model = PaymentAccount
+
+
+@admin.register(ClientInformation)
+class ClientInformationAdminView(admin.ModelAdmin):
+    model = ClientInformation
+
+
+@admin.register(Invoice)
+class InvoiceAdminView(admin.ModelAdmin):
+    model = Invoice
+
+    list_filter = (
+        "due_date",
+        "created_at",
+        "updated_at",
+    )
