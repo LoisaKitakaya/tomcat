ffaffe4dd3651f5557995c1c4055ff18e6ec534f
Freedom Loisa <kitakayaloisa@gmail.com>
2023-06-10 20:53:41 +0300

    minor modification

--- billing/admin.py
+++ billing/admin.py
@@ -1,14 +1,1 @@
 from django.contrib import admin
-from billing.models import PlanBilling
-
-
-@admin.register(PlanBilling)
-class PlanBillingAdminView(admin.ModelAdmin):
-    model = PlanBilling
-
-    list_display = ("merchant_ref",)
-
-    list_filter = (
-        "created_at",
-        "updated_at",
-    )
