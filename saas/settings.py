9332ec710ad41c47ef0888ba437b0f66e7afa7d9
Freedom Loisa <kitakayaloisa@gmail.com>
2023-06-11 16:05:27 +0300

    changed static folder configuration

--- saas/settings.py
+++ saas/settings.py
@@ -128,15 +128,14 @@ USE_I18N = True
 
 USE_TZ = True
 
+CUSTOM_TIME_ZONE = "Africa/Nairobi"
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "static/"
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
-
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 
 # Default primary key field type
