452b92ad1cc428f08acfc22567d2a15f23fb7a91
Freedom Loisa <kitakayaloisa@gmail.com>
2023-06-12 06:39:12 +0300

    whitenoise settings and collecting static files

--- saas/settings.py
+++ saas/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -136,6 +137,14 @@ CUSTOM_TIME_ZONE = "Africa/Nairobi"
 STATIC_URL = "static/"
 
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
+
+# whitenoise settings
+
+STORAGES = {
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
 
 
 # Default primary key field type
