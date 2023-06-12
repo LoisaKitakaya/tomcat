8d2ebb02d7214c7f8ea2dfd53fcb8129636d78f6
Freedom Loisa <kitakayaloisa@gmail.com>
2023-06-12 08:01:17 +0300

    changed db port setting

--- saas/settings.py
+++ saas/settings.py
@@ -94,7 +94,7 @@ DATABASES = {
         "USER": os.getenv("DATABASE_USER"),
         "PASSWORD": os.getenv("DATABASE_PASSWORD"),
         "HOST": os.getenv("DATABASE_HOST"),
-        "PORT": "5432",
+        "PORT": os.getenv("DATABASE_PORT"),
     }
 }
 
